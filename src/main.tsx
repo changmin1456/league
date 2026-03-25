212c164c530d832bbf32e207ed5b73ae078c5c87
윤창민 <changmin@yunchangmin-ui-MacBookAir.local>
2026-03-25 14:54:03 +0900

    fix: serve riot verification code on hash verification route

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const RIOT_VERIFY_CODE = '2dd31827-dc1f-486c-8f9a-a7dd8f261642'
+const isRiotHashVerifyPath = window.location.hash === '#전적/riot.txt'
+
+if (isRiotHashVerifyPath) {
+  document.body.innerText = RIOT_VERIFY_CODE
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
