c57c95c4bca7423b12cd467e9be73b447bbad21b
윤창민 <changmin@yunchangmin-ui-MacBookAir.local>
2026-03-25 15:00:22 +0900

    chore: update riot verification token

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-const RIOT_VERIFY_CODE = '2dd31827-dc1f-486c-8f9a-a7dd8f261642'
+const RIOT_VERIFY_CODE = 'fad8c1da-d06b-43f8-aec6-572e3bb1c18f'
 const isRiotHashVerifyPath = window.location.hash === '#전적/riot.txt'
 
 if (isRiotHashVerifyPath) {
